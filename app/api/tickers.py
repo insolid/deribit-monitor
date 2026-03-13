237d1c9f66cf79c1aceee76b7514ac96e252e8f3
insolid <magalam000@gmail.com>
2026-03-13 13:19:24 +0300

    Create ticker endpoints

---
+++ app/api/tickers.py
@@ -0,0 +1,46 @@
+from fastapi.routing import APIRouter
+from sqlalchemy import select
+from fastapi import HTTPException, Query
+from app.core.db import SessionDep
+from app.models import Ticker
+from app.schemas import Ticker as TickerSchema
+from typing import Literal, Annotated
+from datetime import date, datetime, time, timedelta
+
+router = APIRouter(prefix="/tickers", tags=["tickers"])
+
+
+@router.get("/", response_model=list[TickerSchema])
+async def get_all_prices(db: SessionDep, ticker: str):
+    return await db.scalars(select(Ticker).where(Ticker.symbol == ticker))
+
+
+@router.get("/last-price", response_model=TickerSchema | None)
+async def get_last_price(db: SessionDep, ticker: str):
+    return await db.scalar(
+        select(Ticker)
+        .where(Ticker.symbol == ticker)
+        .order_by(Ticker.timestamp.desc())
+        .limit(1)
+    )
+
+
+@router.get("/by-date", response_model=list[TickerSchema])
+async def get_all_prices_by_date(
+    db: SessionDep,
+    ticker: str,
+    date_from: Annotated[date, Query(example="2025-01-01")],
+    date_to: Annotated[date, Query(example="2025-01-02")],
+):
+    date_from_ts = int(datetime.combine(date_from, time.min).timestamp())
+    date_to_ts = int(
+        datetime.combine(date_to + timedelta(days=1), time.min).timestamp()
+    )
+
+    stmt = select(Ticker).where(
+        Ticker.symbol == ticker,
+        Ticker.timestamp >= date_from_ts,
+        Ticker.timestamp < date_to_ts,
+    )
+
+    return await db.scalars(stmt)
--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
 from fastapi import FastAPI
+from app.api import tickers
 
 app = FastAPI()
+
+app.include_router(tickers.router)
