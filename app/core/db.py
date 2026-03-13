297da21ea1ae66103f7b2186eb0c4ecb2d31c51f
insolid <magalam000@gmail.com>
2026-03-13 12:02:26 +0300

    Setup project structure and config

---
+++ app/core/db.py
@@ -0,0 +1,23 @@
+from typing import Annotated
+
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import (AsyncSession, async_sessionmaker,
+                                    create_async_engine)
+from sqlalchemy.orm import DeclarativeBase
+
+from .config import settings
+
+engine = create_async_engine(settings.postgres_url)
+local_session = async_sessionmaker(engine, expire_on_commit=False, autoflush=False)
+
+
+async def get_db():
+    async with local_session() as session:
+        yield session
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+SessionDep = Annotated[AsyncSession, Depends(get_db)]
